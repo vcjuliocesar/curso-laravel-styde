01bbf676a605d5e52c80fceac4c31b224e2d1bfc
Julio Cesar Valadez Castañeda <juliocesar@iMac-de-Julio.local>
2020-03-16 00:03:53 -0600

    layouts y blade

--- resources/views/notes.blade.php
+++ resources/views/notes.blade.php
@@ -1,33 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Title</title>
-    <link rel="stylesheet" type="text/css" href="{{asset('/static/css/app.css')}}">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-</head>
-<body>
-    <div class="wrap">
-        <header class="head">
-            <a href="#" class="logo"></a>
+@extends('layout')
 
-            <nav class="main-nav">
-                <ul class="main-nav-list">
-                    <li class="main-nav-item active">
-                        <a href="/static/notes.html" class="main-nav-link">
-                            <i class="icon icon-th-list"></i>
-                            <span>Ver notas</span>
-                        </a>
-                    </li>
-                    <li class="main-nav-item">
-                        <a href="/static/add-note.html" class="main-nav-link">
-                            <i class="icon icon-pen"></i>
-                            <span>Nueva nota</span>
-                        </a>
-                    </li>
-                </ul>
-            </nav>
-        </header>
+@section('content')
+
+
         <main class="content">
             <div class="cards">
               @forelse ($notes as $note)
@@ -174,17 +149,4 @@
                 </div>
             </div>
         </main>
-        <footer class="foot">
-            <div class="ad">
-                <p>
-                    Esta aplicación es desarrollada en el curso
-                    <a href="https://styde.net/laravel-6">Primeros pasos con Laravel 6</a>.
-                </p>
-            </div>
-            <div class="license">
-                <p>© 2019 Derechos Reservados - Styde Limited</p>
-            </div>
-        </footer>
-    </div>
-</body>
-</html>
+@endsection
